41786e3978643de82126c0e2fba9a815165bed21
Ben Sorohan <ben.sorohan@bluehype.com.au>
2014-07-10 11:31:17 +1000

    Made reloading vcl optional.

--- manifests/vcl.pp
+++ manifests/vcl.pp
@@ -5,13 +5,22 @@
 # and Varnish will continue to run with the old config
 define varnish::vcl (
   $content,
-  $file = $name) {
+  $file = $name,
+  $reload = true
+) {
 
   include varnish
   include varnish::params
 
+  if $reload {
+    $reload_command = $varnish::params::vcl_reload
+  }
+  else {
+    $reload_command = "/bin/echo 'noop' >> /dev/null"
+  }
+
   exec { "vcl_reload_$name":
-    command     => $varnish::params::vcl_reload,
+    command     => $reload_command,
     refreshonly => true,
   }
 
